8df5a913c6bb37b6724730a3831b7b7805f6646c
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2023-10-03 15:43:31 +0500

    Add files via upload

---
+++ WpfApp5.1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp5._1.Pages;
+
+namespace WpfApp5._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Page[] pages = new Page[]
+        {
+        new RoutingPage(),
+        new AttacheEventsPage(),
+        new TextBoxPage(),
+        new ValidationInputPage()
+        };
+
+        private void PageComboBox_SelectionChanged(object sender,SelectionChangedEventArgs e)
+        {
+
+        }
+        private void combobox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ WpfApp5.1/Pages/RoutingPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5._1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RoutingPage.xaml
+    /// </summary>
+    public partial class RoutingPage : Page
+    {
+        public RoutingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
